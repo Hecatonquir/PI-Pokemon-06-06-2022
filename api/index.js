8ae73fc84aef65548fb30aecc5833d6e36211536
Hecatonquir <maxicuentas92@hotmail.com>
2022-07-16 06:30:42 -0300

    Getting ready for deploy

--- api/index.js
+++ api/index.js
@@ -21,9 +21,9 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { getTypes } = require('./src/functions');
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-	server.listen(3001, async () => {
-		console.log('%s listening at 3001');
+conn.sync({ force: false }).then(() => {
+	server.listen(process.env.PORT, async () => {
+		console.log(`%s listening at ${process.env.PORT}`);
 		await getTypes();
 	});
 });
